f751cc711e8fbf02604e0556aec92aee61a8e56e
Michael Angelo Guarin <mklgrn2328@outlook.com>
2026-04-02 12:48:03 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import type { NextConfig } from ".pnpm/next@16.2.2_@babel+core@7.29.0_react-dom@19.2.4_react@19.2.4__react@19.2.4/node_modules/next";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
